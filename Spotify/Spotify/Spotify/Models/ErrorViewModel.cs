0bbca0dc6b2f883cfd4b09d474dec964ba87c643
mahdis-fathi <88924157+mahdis-fathi@users.noreply.github.com>
2024-01-07 12:09:57 +0330

    creating the project

---
+++ Spotify/Spotify/Spotify/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spotify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
